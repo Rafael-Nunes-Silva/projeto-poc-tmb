fc174bc81955988cbc3050f44efc81940deda4fe
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2025-10-21 21:32:24 -0300

    Modelo da entidade pedido

---
+++ api-poc-tmb/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿namespace api_poc_tmb.Models
+{
+    public enum EOrderStatus
+    {
+        Pendente,
+        Processando,
+        Finalizado
+    }
+
+    public class Order
+    {
+        public int id { get; set; }
+        public string Cliente { get; set; }
+        public string Produto { get; set; }
+        public float Valor { get; set; }
+        public EOrderStatus Status { get; set; }
+        public DateTimeOffset Sata_criacao { get; set; }
+    }
+}
