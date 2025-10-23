e0f0206b6219e5dadacfc11eedea044a9e6481bd
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2025-10-23 20:07:45 -0300

    Registrando os serviços utilizados pelo worker

--- worker-orders/Program.cs
+++ worker-orders/Program.cs
@@ -1,6 +1,19 @@
+using api_poc_tmb.Data;
+using Azure.Messaging.ServiceBus;
+using Microsoft.EntityFrameworkCore;
 using worker_orders;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddSingleton<ServiceBusClient>(sp =>
+{
+    var connString = builder.Configuration.GetSection("AzureServiceBus").GetValue<string>("ConnectionString");
+    return new ServiceBusClient(connString);
+});
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
