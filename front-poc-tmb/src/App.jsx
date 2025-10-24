b741a403058849e7db1fc0868d7e972c6b953d44
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2025-10-23 22:22:24 -0300

    Alterando de CRA para Vite

---
+++ front-poc-tmb/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind!</h1>
+      <p className="mt-4 text-gray-700">If this text is styled, Tailwind is working 🎉</p>
+    </div>
+  );
+}
+
+export default App
