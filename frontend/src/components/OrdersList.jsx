ed722ec081c9add18a0a20d74b9124f23a412bb5
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2025-10-25 16:54:43 -0300

    Indicador de carregamento

--- frontend/src/components/OrdersList.jsx
+++ frontend/src/components/OrdersList.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { GetOrders } from "../services/OrdersService";
 import StatusBadge from "./StatusBadge";
 import { useNavigate } from 'react-router-dom';
+import Loading from "../components/Loading";
 
 export default function OrdersList() {
     const navigate = useNavigate();
 
-    const [orders, setOrders] = useState([]);
+    const [orders, setOrders] = useState();
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -19,33 +20,36 @@ export default function OrdersList() {
 
     return (
         <div className="p-6 space-y-5">
-            {orders.map((order) => (
-                <div
-                    key={order.id}
-                    className="bg-slate-50 border border-slate-200 shadow-sm rounded-lg px-5 py-4"
-                    onClick={() => navigate(`/detalhes-pedido/${order.id}`)}
-                >
-                    <div className="flex justify-between items-center">
-                        <div className="flex flex-wrap items-center text-slate-700 text-sm sm:text-base font-medium gap-2">
-                            <span className="text-slate-400 font-semibold">{order.id}</span>
-                            <span className="text-slate-300">|</span>
-                            <span className="font-semibold text-lg text-slate-800">{order.produto}</span>
-                            <span className="text-slate-300">|</span>
-                            <span className="text-slate-500">{order.cliente}</span>
-                        </div>
-                        <span className="text-slate-500 font-medium text-sm">
-                            {new Date(order.data_criacao).toLocaleDateString("pt-BR")}
-                        </span>
-                    </div>
+            {
+                !orders ? <Loading></Loading> :
+                    orders.map((order) => (
+                        <div
+                            key={order.id}
+                            className="bg-slate-50 border border-slate-200 shadow-sm rounded-lg px-5 py-4"
+                            onClick={() => navigate(`/detalhes-pedido/${order.id}`)}
+                        >
+                            <div className="flex justify-between items-center">
+                                <div className="flex flex-wrap items-center text-slate-700 text-sm sm:text-base font-medium gap-2">
+                                    <span className="text-slate-400 font-semibold">{order.id}</span>
+                                    <span className="text-slate-300">|</span>
+                                    <span className="font-semibold text-lg text-slate-800">{order.produto}</span>
+                                    <span className="text-slate-300">|</span>
+                                    <span className="text-slate-500">{order.cliente}</span>
+                                </div>
+                                <span className="text-slate-500 font-medium text-sm">
+                                    {new Date(order.data_criacao).toLocaleDateString("pt-BR")}
+                                </span>
+                            </div>
 
-                    <div className="flex justify-between items-center mt-3">
-                        <span className="text-xl font-semibold text-slate-800">
-                            R$ {order.valor}
-                        </span>
-                        <StatusBadge status={order.status} />
-                    </div>
-                </div>
-            ))}
+                            <div className="flex justify-between items-center mt-3">
+                                <span className="text-xl font-semibold text-slate-800">
+                                    R$ {order.valor}
+                                </span>
+                                <StatusBadge status={order.status} />
+                            </div>
+                        </div>
+                    ))
+            }
         </div>
     );
 }
--- frontend/src/components/OrdersTable.jsx
+++ frontend/src/components/OrdersTable.jsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { GetOrders } from "../services/OrdersService";
 import StatusBadge from "./StatusBadge";
 import { useNavigate } from 'react-router-dom';
+import Loading from "../components/Loading";
 
 export default function OrdersTable() {
     const navigate = useNavigate();
 
-    const [orders, setOrders] = useState([]);
+    const [orders, setOrders] = useState();
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -19,37 +20,40 @@ export default function OrdersTable() {
 
     return (
         <div className="p-5">
-            <table className="w-full text-left table-auto hidden md:table border border-slate-200 rounded-md">
-                <thead>
-                    <tr className="border-b bg-slate-200">
-                        <th className="p-2">Id</th>
-                        <th className="p-2">Cliente</th>
-                        <th className="p-2">Produto</th>
-                        <th className="p-2">Valor</th>
-                        <th className="p-2">Status</th>
-                        <th className="p-2">Data da criacao</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {orders.map((order, index) =>
-                        <tr
-                            key={index}
-                            className={index % 2 != 0 ? "bg-slate-100" : "bg-white"}
-                            onClick={() => navigate(`/detalhes-pedido/${order.id}`)}
-                        >
-                            <td className="p-3 font-medium">{order.id}</td>
-                            <td className="p-3 font-medium">{order.cliente}</td>
-                            <td className="p-3 font-medium">{order.produto}</td>
-                            <td className="p-3 font-medium">R$ {order.valor}</td>
-                            <td className="p-3 font-medium"><StatusBadge status={order.status}></StatusBadge></td>
-                            <td className="p-3 font-medium">{new Date(order.data_criacao)
-                                .toLocaleString("pt-BR", {
-                                    dateStyle: "short"
-                                })}</td>
+            {!orders ? <Loading></Loading> :
+                <table className="w-full text-left table-auto hidden md:table border border-slate-200 rounded-md">
+                    <thead>
+                        <tr className="border-b bg-slate-200">
+                            <th className="p-2">Id</th>
+                            <th className="p-2">Cliente</th>
+                            <th className="p-2">Produto</th>
+                            <th className="p-2">Valor</th>
+                            <th className="p-2">Status</th>
+                            <th className="p-2">Data da criacao</th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+
+                        {orders.map((order, index) =>
+                            <tr
+                                key={index}
+                                className={index % 2 != 0 ? "bg-slate-100" : "bg-white"}
+                                onClick={() => navigate(`/detalhes-pedido/${order.id}`)}
+                            >
+                                <td className="p-3 font-medium">{order.id}</td>
+                                <td className="p-3 font-medium">{order.cliente}</td>
+                                <td className="p-3 font-medium">{order.produto}</td>
+                                <td className="p-3 font-medium">R$ {order.valor}</td>
+                                <td className="p-3 font-medium"><StatusBadge status={order.status}></StatusBadge></td>
+                                <td className="p-3 font-medium">{new Date(order.data_criacao)
+                                    .toLocaleString("pt-BR", {
+                                        dateStyle: "short"
+                                    })}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            }
         </div>
     );
 }
