3c1bee626792ac865e29e375a2ef8721ed1f635f
Rafael-Nunes-Silva <rafaelnunesilva20@gmail.com>
2025-10-25 15:47:29 -0300

    Navegação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import NewOrder from './pages/NewOrder';
+import Orders from './pages/Orders';
+import OrderDetails from './pages/OrderDetails';
 
 function App() {
   return (
-    <div>
-      <NewOrder></NewOrder>
-    </div>
+    <Router>
+      <nav className="p-5 flex flex-row gap-3 bg-gray-200">
+        <Link className="bg-gray-400 font-semibold p-1 rounded-md"
+          to="/novo-pedido"
+        >Novo pedido</Link>
+        <Link className="bg-gray-400 font-semibold p-1 rounded-md"
+          to="/listagem-pedidos"
+        >Pedidos</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/novo-pedido" element={<NewOrder />} />
+        <Route path="/listagem-pedidos" element={<Orders />} />
+        <Route path="/detalhes-pedidos" element={<OrderDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
--- frontend/src/pages/NewOrder.jsx
+++ frontend/src/pages/NewOrder.jsx
@@ -2,7 +2,7 @@ import OrderForm from "../components/OrderForm";
 
 export default function NewOrder() {
     return (
-        <div>
+        <div className="mx-auto mt-50">
             <OrderForm></OrderForm>
         </div>
     );
